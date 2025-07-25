5e54cf19525c1cef5b05a30da4b367cd9e4ee454
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-07-25 19:04:16 +0300

    Отобразил на странице список задач

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
 import "./styles/styles.css"
+
+// Импортирует список задач
+import {todos} from "./utils/constants"
+
+// HTML список дел
+const contentElement = document.querySelector(".todos__list");
+
+// HTML шаблон
+const template = document.querySelector("#todo-item-template") as HTMLTemplateElement;
+
+// Принимает строку, клонирует шаблон элемента списка,
+// записывает в заголовок списка переданную строку,
+// возвращает заполненный элемент списка
+function createElement(item: string) {
+    const itemElement = template.content.querySelector(".todo-item").cloneNode(true) as HTMLElement;
+    const title = itemElement.querySelector(".todo-item__text");
+    title.textContent = item;
+    return itemElement;
+}
+
+// Перебирает массив с названиями дел, и вставляет названия в элементы списка, добавляет их в список на страницу
+todos.forEach(item => {
+    const itemElement = createElement(item);
+    contentElement.prepend(itemElement);
+})
+
