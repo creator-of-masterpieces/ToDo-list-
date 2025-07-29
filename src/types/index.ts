09c88acd4b7e94bdea0e06c26ac0fe245b35f552
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-07-29 20:56:38 +0300

    Написал тип для хранилища данных элементов списка

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,15 @@
+// Элемент списка.
+// Хранит id и заголовок элемента списка
 export interface IItem {
     id: string;
     name: string;
+}
+
+// Хранилище данных элементов списка.
+// Хранит массив элементов списка.
+// Имеет методы для добавления и удаления элементов из хранилища
+export interface IToDoModel {
+    items: IItem[];
+    addItem: (data: string) => IItem;
+    removeItem: (id: string) => void;
 }
