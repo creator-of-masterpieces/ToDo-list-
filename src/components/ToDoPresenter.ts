54be925b8f9e2e84c3d469a29df8829fcb90f84c
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-08-07 21:02:22 +0300

    Добавил класс презентер и комменты к нему; Изменил index.ts, надо дописать комменты;

--- src/components/ToDoPresenter.ts
+++ src/components/ToDoPresenter.ts
@@ -3,24 +3,65 @@ import {IToDoModel} from "../types";
 import {IPage} from "./Page";
 import {IViewitemConstructor} from "./Item";
 
+// Класс-презентер отвечает за взаимодействие модели данных и визуального интерфейса.
+// Он координирует логику: создает формы, обрабатывает события и обновляет отображение.
 export class ItemPresenter {
+    // Шаблон элемента задачи
     protected itemTemplate: HTMLTemplateElement;
+    // Шаблон формы
     protected formTemplate: HTMLTemplateElement;
+    // Экземпляр формы добавления задачи
     protected todoForm: IForm;
+    // Экземпляр формы редактирования задачи (в текущей реализации не используется)
     protected todoEditForm: IForm;
 
     constructor(
+        // Интерфейс модели данных
         protected model: IToDoModel,
+        // Интерфейс конструктора формы, будет использоваться для создания новых форм
         protected formConstructor: IFormConstructor,
+        // Интерфейс класса для управления отображением страницы
         protected viewPageContainer: IPage,
+        // Интерфейс конструктора элемента списка дел
         protected viewItemConstructor: IViewitemConstructor,
     ) {
+        // Получаем шаблон элемента списка задач из разметки
         this.itemTemplate = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+        // Получаем шаблон формы добавления задачи из разметки
         this.formTemplate = document.querySelector('#todo-form-template') as HTMLTemplateElement;
     }
     init() {
+        // Создает экземпляр формы
         this.todoForm = new this.formConstructor(this.formTemplate);
+        // Устанавливает обработчик события отправки для формы
         this.todoForm.setHandler(this.handleSubmitForm.bind(this));
+        // Создает разметку формы и сохраняет её в контейнер страницы
         this.viewPageContainer.formContainer = this.todoEditForm.render();
+    }
+
+    // Обработчик события отправки формы.
+    // Принимает название дела (строку)
+    handleSubmitForm(data: string) {
+        // Создает объект нового дела, добавляет его в массив дел
+        this.model.addItem(data);
+        // Перерисовывает интерфейс (обновляет список задач)
+        this.renderView();
+        // Очищает поля формы после добавления задачи
+        this.todoForm.clearValue();
+    }
+
+    // Отрисовывает список дел
+    renderView() {
+        // Преобразует каждый элемент из модели в визуальный HTML-элемент
+        const itemList = this.model.items.map((item)=> {
+            // создаём визуальный элемент задачи
+            const todoItem = new this.viewItemConstructor(this.itemTemplate);
+            // создаём DOM-элемент
+            const itemElement = todoItem.render(item);
+            return itemElement;
+        }).reverse(); // инвертируем список, чтобы новые задачи отображались выше
+
+        // Устанавливаем новый список задач в контейнер представления страницы
+        this.viewPageContainer.todoContainer = itemList;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,13 @@ import {Item} from "./components/Item";
 import {Form} from "./components/Form";
 import {ToDoModel} from "./components/ToDoModel";
 import {Page} from "./components/Page";
+import {ItemPresenter} from "./components/ToDoPresenter";
 
 // Находим контейнер, в который будем отрисовывать форму и список дел
 const contentElement = document.querySelector('.content') as HTMLElement;
 
-// Получаем шаблон элемента списка задач
-const itemTemplate = document.querySelector('#todo-item-template') as HTMLTemplateElement;
-
-// Получаем шаблон формы добавления задачи
-const formTemplate = document.querySelector('#todo-form-template') as HTMLTemplateElement;
-
 // Создаём экземпляр класса Page, который управляет структурой страницы
-const page = new Page(contentElement);
+const itemContainer = new Page(contentElement);
 
 // Создаём экземпляр модели Tоdo — будет хранить и управлять данными задач
 const todoArray = new ToDoModel();
@@ -27,44 +22,7 @@ const todoArray = new ToDoModel();
 // Инициализируем модель начальными задачами, импортированными из constants.ts
 todoArray.items = todos;
 
-// Создаём экземпляр формы для добавления новых задач
-const todoForm = new Form(formTemplate);
+const itemPresentor = new ItemPresenter(todoArray, Form, itemContainer, Item);
 
-// Устанавливаем обработчик события отправки формы
-todoForm.setHandler(handleSubmitForm);
-
-// Добавляем сгенерированный элемент формы на страницу
-page.formContainer = todoForm.render();
-
-/**
- * Обработчик оправки формы
- * @param data - строка, введённая пользователем
-/* Добавляет новую задачу в список,
- * очищает поле ввода
- * и заново отрисовывает все задачи
- */
-function handleSubmitForm(data: string) {
-    todoArray.addItem(data);
-    todoForm.clearValue();
-    renderTodoItems();
-}
-
-/**
- * Отрисовка всех задач
- *
- * Перебирает массив задач из todoArray,
- * для каждой создаёт объект Item,
- * генерирует для него HTML-элемент с текстом задачи,
- * возвращает массив этих элементов и отображает их на странице в обратном порядке (последние сверху)
-*/
-function renderTodoItems() {
-    page.todoContainer = todoArray.items.map(item => {
-        const todoItem = new Item(itemTemplate);
-        const itemElement = todoItem.render(item);
-        return (itemElement);
-    }).reverse()
-}
-
-// Первичная отрисовка задач при загрузке страницы
-renderTodoItems();
-
+itemPresentor.init();
+itemPresentor.renderView();
